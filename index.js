15c1bd57f991e481f97b995c6b862890a022ece1
Aurelien-DRUON <aurelien.druon@epfedu.fr>
2024-10-09 11:53:54 +0200

    npm start + route objectives

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// server.js
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!\n");
+  res.redirect("/objectives");
+});
+
+app.get("/objectives", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pages/objectives.html"));
 });
 
 app.listen(port, "127.0.0.1", () => {
-  console.log(`Listening on 127.0.0.1:${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
-
-// run with `node server.js`
