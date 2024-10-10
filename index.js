05117283edb543d2792f75f692b59948568d7b34
Aurelien-DRUON <aurelien.druon@epfedu.fr>
2024-10-10 16:04:27 +0200

    ça marche pas

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = 3000;
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-app.get("/", (req, res) => {
-  res.redirect("/objectives");
-});
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+    </div>
+  );
+};
 
-app.get("/objectives", (req, res) => {
-  res.sendFile(path.join(__dirname, "./pages/objectives.html"));
-});
-
-app.listen(port, "127.0.0.1", () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
